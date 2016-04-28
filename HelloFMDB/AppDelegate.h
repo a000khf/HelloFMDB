c1ebd4c8ac777b1cf7e4cef7a45a03f6613c3e8c
a000khf <fu.zigan@163.com>
2016-04-29 00:32:58 +0800

    Initial Commit

---
+++ HelloFMDB/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  HelloFMDB
+//
+//  Created by zhu on 16/4/29.
+//  Copyright © 2016年 zhu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
